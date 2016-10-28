e2060a423b07e464c25acd9dce0d303f71d20070
shijingui <shijingui@jd.com>
2016-10-28 17:37:25 +0800

    memory

---
+++ src/main/java/org/masksalt/domain/MemoryObject.java
@@ -0,0 +1,8 @@
+package org.masksalt.domain;
+
+/**
+ * User: shijingui
+ * Date: 2016/10/28
+ */
+public class MemoryObject {
+}
---
+++ src/main/java/org/masksalt/jvm/TestFullGc.java
@@ -0,0 +1,19 @@
+package org.masksalt.jvm;
+
+import org.masksalt.domain.MemoryObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: shijingui
+ * Date: 2016/10/28
+ */
+public class TestFullGC {
+    public static void main(String[] args) {
+        List<MemoryObject> objectList = new ArrayList<MemoryObject>(6);
+
+
+    }
+
+}
