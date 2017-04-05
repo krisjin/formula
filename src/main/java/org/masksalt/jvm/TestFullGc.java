98073c507cf242a9cec4f8cb6f109d4e5159d79c
krisjin <krisibm@163.com>
2017-04-05 23:55:51 +0800

    string

--- src/main/java/org/masksalt/jvm/TestFullGc.java
+++ src/main/java/org/masksalt/jvm/TestFullGc.java
@@ -9,7 +9,7 @@ import java.util.List;
  * User: shijingui
  * Date: 2016/10/28
  */
-public class TestFullGC {
+public class TestFullGc {
     public static void main(String[] args) {
         List<MemoryObject> objectList = new ArrayList<MemoryObject>(6);
 
