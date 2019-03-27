09b63b2ae7ac7310a6389a562185ca31c34a8738
krisjin <krisibm@163.com>
2019-03-28 06:51:10 +0800

    decorator

---
+++ src/main/java/org/strelitzia/pettern/decorator/Decorator.java
@@ -0,0 +1,11 @@
+package org.strelitzia.pettern.decorator;
+
+/**
+ * @author krisjin on 2019/3/28
+ */
+public class Decorator implements Component {
+    @Override
+    public String handle() {
+        return null;
+    }
+}
