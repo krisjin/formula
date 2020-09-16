1bde6aab9bd29ccadf1f375114500fd57e027e6d
krisjin <krisibm@163.com>
2020-09-16 15:34:27 +0800

    update

---
+++ src/main/java/org/formula/bytecode/Account.java
@@ -0,0 +1,17 @@
+package org.formula.bytecode;
+
+/**
+ * User:krisjin
+ * Date:2020-05-07
+ */
+public class Account {
+    private final String name;
+    private double money;
+
+    //构造函数初始化
+    public Account() {
+        this.name = "zhangsan";
+        this.money = 111.11;
+    }
+
+}
