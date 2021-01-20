4eeeaba9d995b2790c0411f2ffba83904ac7e826
krisjin <krisibm@163.com>
2021-01-20 09:17:26 +0800

    update

--- src/main/java/org/formula/nio/ChannelTest.java
+++ src/main/java/org/formula/nio/ChannelTest.java
@@ -2,7 +2,9 @@ package org.formula.nio;
 
 import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 /**
  * @author krisjin
@@ -10,22 +12,30 @@ import java.nio.channels.FileChannel;
  */
 public class ChannelTest {
 
-
     public static void main(String[] args) {
-
+        channelTest();
     }
 
-
     public static void channelTest() {
-
         RandomAccessFile raf = null;
         try {
+//            String t = "666666\n";
             //创建一个RandomAccessFile（随机访问文件）对象，
-            raf = new RandomAccessFile("D:\\test.txt", "rw");
+            raf = new RandomAccessFile("/usr/local/tools/a.txt", "rw");
 
             //通过RandomAccessFile对象的getChannel()方法。FileChannel是抽象类。
             FileChannel inChannel = raf.getChannel();
-        } catch (FileNotFoundException e) {
+            ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+
+            for (int i = 0; i < 30; i++) {
+                byteBuffer.put((i + "").getBytes("UTF-8"));
+            }
+
+//            byteBuffer.put(t.getBytes("UTF-8"));
+            byteBuffer.flip();
+            inChannel.write(byteBuffer);
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
