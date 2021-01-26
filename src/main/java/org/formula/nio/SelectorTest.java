97d772393c3afc6ab2037c15cca67dff049c6c3a
krisjin <krisibm@163.com>
2021-01-26 10:15:33 +0800

    Socket test

---
+++ src/main/java/org/formula/nio/SelectorTest.java
@@ -0,0 +1,25 @@
+package org.formula.nio;
+
+import java.nio.channels.SelectionKey;
+
+/**
+ * @author krisjin
+ * @date 2021/1/25
+ */
+public class SelectorTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void tt() {
+
+        //选择器监控通道的IO事件类型
+        int connect = SelectionKey.OP_CONNECT;
+        int accept = SelectionKey.OP_ACCEPT;
+        int read = SelectionKey.OP_READ;
+        int write = SelectionKey.OP_WRITE;
+
+
+    }
+}
