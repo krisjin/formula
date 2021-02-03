9d529845aac73bf12e785c850b3f58c55837e8e6
krisjin <krisibm@163.com>
2021-02-03 22:45:58 +0800

    delete class

--- src/main/java/org/formula/nio/TT.java
+++ src/main/java/org/formula/nio/TT.java
@@ -1,5 +1,7 @@
 package org.formula.nio;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -9,13 +11,38 @@ import java.util.Random;
 public class TT {
 
     public static void main(String[] args) {
-    t();
+        t();
     }
 
     public static void t() {
         Random rnd = new Random();
         boolean toBe = rnd.nextBoolean();
-        Number result = (toBe || !toBe) ?  new Integer(3) : new Float(1);
+        Number result = (toBe || !toBe) ? new Integer(3) : new Float(1);
         System.out.println(result);
+    }
+
+
+    public static void tt(String[] args) {
+        Map map = new IdentityHashMap<>();
+        map.put(1, "Hello");
+        map.putIfAbsent(1, "World");
+        print(map.get(1));
+        print(map.size());
+        Integer i = 1024;
+        map.put(i, "A");
+        map.putIfAbsent(1024, "B");
+        print(map.get(i));
+        print(map.size());
+
+
+    }
+
+
+    private static void print(Object object) {
+        System.out.print(object + " ");
+    }
+
+    public static Integer parseInt(String s) {
+        return (s == null) ? (Integer) null : Integer.parseInt(s);
     }
 }
