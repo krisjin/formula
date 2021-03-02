587608d6d695e62992294d45cdd926947c26a72f
krisjin <krisibm@163.com>
2021-03-02 14:57:46 +0800

    clean

---
+++ src/main/java/org/formula/base/StringIntegerCacheTest.java
@@ -0,0 +1,21 @@
+package org.formula.base;
+
+/**
+ * User: krisjin
+ * Date: 2021/3/2
+ */
+public class StringIntegerCacheTest {
+
+    public static void testInteger() {
+        Integer i1 = new Integer(1);
+        Integer i2 = new Integer(1);
+        System.out.println(2 & 2);
+    }
+
+    /**
+     * 测试String字符串缓存，
+     */
+    public static void testString() {
+
+    }
+}
--- src/main/java/org/formula/bytecode/FullVolatileVisibility.java
+++ src/main/java/org/formula/bytecode/FullVolatileVisibility.java
@@ -1,6 +1,8 @@
 package org.formula.bytecode;
 
 /**
+ * 测试Volatile关键字特性
+ *
  * 当程序执行到volatile变量的读操作或者写操作时，在其前面的操作的更改肯定全部已经进行，且结果已经对后面的操作可见；在其后面的操作肯定还没有进行；
  * 在进行指令优化时，不能将在对volatile变量访问的语句放在其后面执行，也不能把volatile变量后面的语句放到其前面执行。
  * User:krisjin
